052e1b189aff282cc769cced3b684977ebe6efa9
vsantsal <vsantsal@gmail.com>
2022-03-12 14:12:30 -0300

    Adicionando novos testes funcionais correspondentes aos cenários; os últimos ainda marcados para serem pulados, para avaliar melhor forma de utilizar recursos por meio do selenium.

--- testes_funcionais.py
+++ testes_funcionais.py
@@ -1,13 +1,9 @@
 import time
-from collections import namedtuple
 from django.test import tag
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from typing import List
+from typing import Dict, List
 import unittest
-
-
-DadosEntrada = namedtuple('DadosEntrada', 'valor atributo_html valor_atributo_html id_html')
 
 
 class CadastroDeNovoUsuarioTest(unittest.TestCase):
@@ -15,21 +11,28 @@ class CadastroDeNovoUsuarioTest(unittest.TestCase):
         self._url_cadastro: str = 'http://localhost:8000/cadastro'
         self.browser = webdriver.Firefox()
         self.id_submissao_dados: str = 'confirmar_cadastro'
-        self.dados_email_padrao: DadosEntrada = DadosEntrada(valor='fulana@meuemail.com',
-                                                             atributo_html='placeholder',
-                                                             valor_atributo_html='E-mail',
-                                                             id_html='email')
-        self.dados_senha_padrao: DadosEntrada = DadosEntrada(valor='123abcABC',
-                                                             atributo_html='placeholder',
-                                                             valor_atributo_html='Senha',
-                                                             id_html='senha')
-        self.dados_confirmacao_senha_padrao: DadosEntrada = DadosEntrada(valor='123abcABC',
-                                                                         atributo_html='placeholder',
-                                                                         valor_atributo_html='Confirmar senha',
-                                                                         id_html='confirmar_senha')
-        self.dados_entrada: List[DadosEntrada] = [self.dados_email_padrao,
-                                                  self.dados_senha_padrao,
-                                                  self.dados_confirmacao_senha_padrao]
+        self.dados_email_padrao: Dict[str, str] = {
+            'valor': 'fulana@meuemail.com',
+            'atributo_html': 'placeholder',
+            'valor_atributo_html': 'E-mail',
+            'id_html': 'email',
+
+        }
+        self.dados_senha_padrao: Dict[str, str] = {
+            'valor': '123abcABC',
+            'atributo_html': 'placeholder',
+            'valor_atributo_html': 'Senha',
+            'id_html': 'senha',
+        }
+        self.dados_confirmacao_senha_padrao: Dict[str, str] = {
+            'valor': '123abcABC',
+            'atributo_html': 'placeholder',
+            'valor_atributo_html': 'Confirmar senha',
+            'id_html': 'confirmar_senha',
+        }
+        self.dados_entrada: List[Dict[str, str]] = [self.dados_email_padrao,
+                                                    self.dados_senha_padrao,
+                                                    self.dados_confirmacao_senha_padrao]
 
     def tearDown(self) -> None:
         self.browser.quit()
@@ -47,54 +50,99 @@ class CadastroDeNovoUsuarioTest(unittest.TestCase):
         # 1. E-mail
         # 2. Senha
         # 3. Confirmar senha
-        input_email = self.browser.find_element_by_id(self.dados_email_padrao.id_html)
+        input_email = self.browser.find_element_by_id(self.dados_email_padrao['id_html'])
         self.assertIsNotNone(input_email)
-        input_senha = self.browser.find_element_by_id(self.dados_senha_padrao.id_html)
+        input_senha = self.browser.find_element_by_id(self.dados_senha_padrao['id_html'])
         self.assertIsNotNone(input_senha)
-        input_confirmar_senha = self.browser.find_element_by_id(self.dados_confirmacao_senha_padrao.id_html)
+        input_confirmar_senha = self.browser.find_element_by_id(self.dados_confirmacao_senha_padrao['id_html'])
         self.assertIsNotNone(input_confirmar_senha)
 
         # Satisfeita, ela decide inputar seus dados para fazer o cadastro na plataforma
         for dado in self.dados_entrada:
-            inputbox = self.browser.find_element_by_id(dado.id_html)
-            inputbox.send_keys(dado.valor)
+            inputbox = self.browser.find_element_by_id(dado['id_html'])
+            inputbox.send_keys(dado['valor'])
             self.assertEqual(
-                inputbox.get_attribute(dado.atributo_html),
-                dado.valor_atributo_html
+                inputbox.get_attribute(dado['atributo_html']),
+                dado['valor_atributo_html']
             )
 
         # Após inserir os dados, ela confirma o cadastro
         inputbox = self.browser.find_element_by_id(self.id_submissao_dados)
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(2)
+        time.sleep(1)
 
         # Após confirmação, ela é redirecionada para página de login
         self.assertIn("login", self.browser.title.lower())
         self.assertIn("login", self.browser.current_url)
         # Ela sai do navegador para apresentar em paz sua dissertação de mestrado!
 
-    # @unittest.skip('later')
     @tag('teste_funcional')
     def test_nao_deve_redirecionar_para_pagina_de_login_se_pelo_menos_um_campo_estiver_vazio(self):
-        self._helper_cenario_nao_preenchimento_de_campo(self.dados_senha_padrao.id_html)
-        self._helper_cenario_nao_preenchimento_de_campo(self.dados_confirmacao_senha_padrao.id_html)
-        self._helper_cenario_nao_preenchimento_de_campo(self.dados_email_padrao.id_html)
+        self.browser.get(self._url_cadastro)
+        self._helper_cenario_com_preenchimento_invalido_de_campo_customizado(
+            self.dados_senha_padrao['id_html'])
+        self._helper_cenario_com_preenchimento_invalido_de_campo_customizado(
+            self.dados_confirmacao_senha_padrao['id_html'])
+        self._helper_cenario_com_preenchimento_invalido_de_campo_customizado(
+            self.dados_email_padrao['id_html'])
 
-    def _helper_cenario_nao_preenchimento_de_campo(self, id_campo_vazio: str):
-        # Fulana acessa página de cadastro da AluraPic desejando testar se pode se cadastrar
-        # sem preencher um de seus campos
+    @tag('teste_funcional')
+    def test_nao_deve_redirecionar_para_pagina_de_login_se_padrao_email_invalido(self):
+        self.browser.get(self._url_cadastro)
+        emails_invalidos = ('fulanameuemail.com',
+                            'fulana@meuemail',
+                            'fulana',
+                            'fulana@fulana@meuemail.com')
+
+        for valor in emails_invalidos:
+            self._helper_cenario_com_preenchimento_invalido_de_campo_customizado(
+                self.dados_email_padrao['id_html'],
+                valor
+            )
+
+    @unittest.skip('Sob investigação - unclosed sockets')
+    @tag('teste_funcional')
+    def test_nao_deve_redirecionar_para_pagina_de_login_se_senha_e_confirmacao_diferentes(self):
+        self.dados_confirmacao_senha_padrao['valor'] = '456abcABC'
+        self._helper_cenario_com_preenchimento_invalido_de_campo_customizado(
+            self.dados_senha_padrao['id_html'],
+            self.dados_senha_padrao['valor']
+        )
+
+    @unittest.skip('Sob investigação - unclosed sockets')
+    @tag('teste_funcional')
+    def test_nao_deve_redirecionar_para_pagina_de_login_se_senha_invalida(self):
+        senhas_invalidas = ('1234567',
+                            '123abc')
+
+        for valor in senhas_invalidas:
+            self.dados_confirmacao_senha_padrao['valor'] = valor
+            self._helper_cenario_com_preenchimento_invalido_de_campo_customizado(
+                self.dados_senha_padrao['id_html'],
+                valor
+            )
+
+    def _helper_cenario_com_preenchimento_invalido_de_campo_customizado(self,
+                                                                        id_campo_customizado: str,
+                                                                        valor_campo_customizado: str = ''):
+
+        # Fulana acessa página de cadastro da AluraPic desejando testar preenchimentos
+        # inválidos
         self.browser.get(self._url_cadastro)
 
-        # Ela preenche os demais campos que não o campo em teste a ficar vazio
         for dado in self.dados_entrada:
-            if dado.id_html != id_campo_vazio:
-                inputbox = self.browser.find_element_by_id(dado.id_html)
-                inputbox.send_keys(dado.valor)
+            if dado['id_html'] == id_campo_customizado and valor_campo_customizado == '':
+                continue
+            inputbox = self.browser.find_element_by_id(dado['id_html'])
+            if dado['id_html'] == id_campo_customizado:
+                inputbox.send_keys(valor_campo_customizado)
+            else:
+                inputbox.send_keys(dado['valor'])
 
         # Após inserir os dados, ela tenta confirmar o cadastro
         inputbox = self.browser.find_element_by_id(self.id_submissao_dados)
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(2)
+        time.sleep(1)
 
         # E verifica que continuou na página
         self.assertIn("cadastro", self.browser.title.lower())
